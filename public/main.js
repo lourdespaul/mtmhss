8ed5ad7ddd8b6c24395450795622b3f628952caa
lourdespaul <lourdespaulagilan@gmail.com>
2017-07-21 17:08:35 +0530

    sms view

--- public/main.js
+++ public/main.js
@@ -19,6 +19,29 @@ $(document).ready(function() {
                 }, this);
             }
         });
+        $('select[name=sms-standard]').on('change',function(){
+        $.ajax({
+            type:'GET',
+            url:'register/section/'+this.value,
+            dataType: 'json',
+            success: function(data){
+                var select = document.getElementById('sms-section');
+                select.options.length = 0;
+                $('select').material_select();
+                var option = document.createElement('option')
+                opt.value = "ALL";
+                opt.innerHTML="ALL";
+                select.appendChild(option);
+                data.section.forEach(function(element) {
+                    console.log(element);
+                    var opt = document.createElement('option');
+                    opt.value = element._id;
+                    opt.innerHTML = element.code;
+                    select.appendChild(opt);
+                    $('select').material_select();
+                }, this);
+            }
+        });
     });
 });
        
