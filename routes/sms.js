064237cf741356bfa5c9724b37e7e95d131ef73c
lourdespaul <lourdespaulagilan@gmail.com>
2017-07-22 20:38:16 +0530

    update

--- routes/sms.js
+++ routes/sms.js
@@ -47,13 +47,13 @@ router.post('/', (req, res)=>{
                 student.forEach(function(element){
                     numbers.push(element.phone);
                 }, this);
-                res.send(numbers.length());
-                // msg91.sendMultiple("153760A7ehQ8Uc5926af23",numbers,data.text,"MTMHSS","4","91",function(response){
+                // res.send(numbers.length());
+                msg91.sendMultiple("153760A7ehQ8Uc5926af23",numbers,data.text,"MTMHSS","4","91",function(response){
  
-                    //Returns Message ID, If Sent Successfully or the appropriate Error Message 
-                    // if(response) res.send({response: response, message: "Sent successfully"})
+                    // Returns Message ID, If Sent Successfully or the appropriate Error Message 
+                    if(response) res.send({response: response, message: "Sent successfully"})
                 
-                // });
+                });
 
             });
         }
